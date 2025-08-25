742ea6d7062b245ce811a3d57da97cb92892eed1
Saravanaff <sarovanq@gmail.com>
2025-08-25 13:54:14 +0530

    model problem solved

--- backend/db.ts
+++ backend/db.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/User';
-import { Exam } from "../backend/models/Exam";
+import { Exam } from "../backend/models/Exam";;
 import { Attend } from "./models/Attend";
+import {Scores} from "./models/Scores"
 export const sequelize = new Sequelize({
   database: 'test',
   dialect: 'mysql',
   username: 'root',
   password: '',
-  models: [User, Exam, Attend],
+  models: [User, Exam, Attend,Scores],
 });
