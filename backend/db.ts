3b158492626d76bc016d8ae0e28ae3678a8d73f8
Saravanaff <sarovanq@gmail.com>
2025-09-12 00:47:20 +0530

    Added log model in DB for storing user Logs

--- backend/db.ts
+++ backend/db.ts
@@ -3,11 +3,12 @@ import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/User';
 import { Exam } from "../backend/models/Exam";;
 import { Attend } from "./models/Attend";
-import {Scores} from "./models/Scores"
+import {Scores} from "./models/Scores";
+import { ViolationLog } from "./models/ViolationLog";
 export const sequelize = new Sequelize({
   database: 'test',
   dialect: 'mysql',
   username: 'root',
   password: '',
-  models: [User, Exam, Attend,Scores],
+  models: [User, Exam, Attend,Scores,ViolationLog],
 });
