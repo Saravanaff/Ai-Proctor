a6585336583eee86dffef218e2da3ffeb4d5fec2
RaghulSub <raghulsubramanian10@gmail.com>
2025-11-09 14:51:22 +0530

    chore: question,questionOption added to sequelize list

--- backend/db.ts
+++ backend/db.ts
@@ -1,18 +1,20 @@
 import "reflect-metadata";
 import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/User';
-import { Exam } from "../backend/models/Exam";;
+import { Exam } from "./models/Exam";
 import { Attend } from "./models/Attend";
-import {Scores} from "./models/Scores";
+import { Scores } from "./models/Scores";
 import { ViolationLog } from "./models/ViolationLog";
+import { Question } from "./models/Questions";
+import { QuestionOption } from "./models/QuestionOption";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const sequelize = new Sequelize({
-  database: process.env.DB_NAME ,
+  database: process.env.DB_NAME,
   dialect: 'mysql',
-  username: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
-  models: [User, Exam, Attend,Scores,ViolationLog],
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  models: [User, Exam, Attend, Scores, ViolationLog, Question, QuestionOption],
 });
